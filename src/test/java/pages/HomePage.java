7e3740142f671baf6d96b257b54eb0f20e759777
Saha <2477357@cognizant.com>
2026-05-13 10:35:10 +0530

    Updated HomePage.java

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,23 +1,34 @@
 package pages;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
-public class HomePage {
-	
+public class HomePage
+{
 	WebDriver driver;
 	WebDriverWait wait;
+	static Logger log = LogManager.getLogger(HomePage.class);
 	
-	public HomePage(WebDriver driver, WebDriverWait wait) {
+	public HomePage(WebDriver driver, WebDriverWait wait)
+	{
 		this.driver = driver;
 		this.wait = wait;
-		PageFactory.initElements(driver, this);
+		PageFactory.initElements(this.driver, this);
 	}
 	
-
+	@FindBy(xpath="//summary[@aria-label='Search']//span")
+	WebElement searchButton;
+	
+	@FindBy(id="Search-In-Modal")
+	WebElement searchBox;
+	
 	@FindBy(xpath = "//div[contains(@class, 'logo')]//img[contains(@alt,'99Bookstore')]")
     WebElement logo;
 
@@ -29,6 +40,33 @@ public class HomePage {
 
     @FindBy(id = "cart-icon-bubble")
     WebElement cartIcon;
+    
+    @FindBy(id = "HeaderMenu-product-categories")
+	WebElement productCategoriesTab;
+    
+    @FindBy(id = "HeaderMenu-bulk-purchase")
+	WebElement bulkPurchaseTab;   
+    
+    @FindBy(id = "HeaderMenu-about-us")
+	WebElement AboutUsTab;
+    
+    @FindBy(id = "HeaderMenu-return-replacement")
+	WebElement ReturnAndReplacementTab;
+    
+    @FindBy(id = "HeaderMenu-contact-us")
+	WebElement 	ContactUsTab;
+    
+    @FindBy(id = "HeaderMenu-terms-conditions")
+	WebElement 	TermsAndConditionsTab;
+    
+    @FindBy(id = "HeaderMenu-privacy-policy")
+	WebElement 	PrivacyPolicyTab;
+    
+    @FindBy(xpath = "//li[contains(@class, 'ai')]//a[contains(@href, 'business')]")
+    WebElement BusinessAndFinanceTab;
+    
+    @FindBy(xpath = "//li[contains(@class, 'ai')]//a[contains(@href, 'mythology')]")
+    WebElement MythologyTab;
     
 
     public boolean isLogoDisplayed() {
@@ -46,6 +84,70 @@ public class HomePage {
 
     public boolean isCartIconDisplayed() {
         return cartIcon.isDisplayed();
-    }	
-
+    }
+    
+    public void verifyProductCategoriesTab() {
+    		productCategoriesTab.click();
+    		String title = driver.getTitle();
+    		Assert.assertEquals(title, "Collections – 99Bookstore");	
+    }
+    
+    public void verifyBulkPurchaseTab() {
+		bulkPurchaseTab.click();
+		String title = driver.getTitle();
+		Assert.assertEquals(title, "Bulk Purchase – 99Bookstore");
+    }
+    
+    public void verifyAboutUsTab() {
+    	  AboutUsTab.click();
+    	  String title = driver.getTitle();
+    	  Assert.assertEquals(title, "About Us – 99Bookstore");
+    }
+    
+    public void verifyReturnAndReplacementTab()
+    {
+    	ReturnAndReplacementTab.click();
+    	  String title = driver.getTitle();
+    	  Assert.assertEquals(title, "Refund policy – 99Bookstore");
+    }
+    
+    public void verifyContactUsTab()
+    {
+    	ContactUsTab.click();
+    	  String title = driver.getTitle();
+    	  Assert.assertEquals(title, "Contact information – 99Bookstore");
+    }
+    
+    public void verifyTermsAndConditionsTab()
+    {
+    	TermsAndConditionsTab.click();
+    	  String title = driver.getTitle();
+    	  Assert.assertEquals(title, "Terms of service – 99Bookstore");
+    }
+    
+    public void verifyPrivacyPolicyTab()
+    {
+    	PrivacyPolicyTab.click();
+    	  String title = driver.getTitle();
+    	  Assert.assertEquals(title, "Privacy policy – 99Bookstore");
+    }
+    public void businessAndFinanceTab()
+    {
+    	BusinessAndFinanceTab.click();
+    	  String title = driver.getTitle();
+    	  Assert.assertEquals(title, "Business and Finance – 99Bookstore");
+    }
+    public void mythologyTab()
+    {
+    	  MythologyTab.click();
+    	  String title = driver.getTitle();
+    	  Assert.assertEquals(title,"Mythology – 99Bookstore");
+    }
+      
+		
+	public void searchText(String text)
+	{
+		searchButton.click();	
+		searchBox.sendKeys(text,Keys.ENTER);
+	}
 }
